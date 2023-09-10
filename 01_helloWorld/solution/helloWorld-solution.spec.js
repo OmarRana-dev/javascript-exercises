c162848ff6218ed641f351ee6719fd18517c3fd1
Omar Rana <sharifzada586@gmail.com>
2023-09-10 11:33:51 +0500

    Solve the problem by writeing Hello, World! at function

--- 01_helloWorld/solution/helloWorld-solution.spec.js
+++ 01_helloWorld/solution/helloWorld-solution.spec.js
@@ -1,7 +1,7 @@
 const helloWorld = require('./helloWorld-solution');
 
 describe('Hello World', function () {
-  test('says "Hello, World!"', function () {
+  test('says "Hello, world!"', function () {
     expect(helloWorld()).toEqual('Hello, World!');
   });
 });
