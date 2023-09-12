a2b5899ebae1abd495b17a8851c7c7b382fcd605
Omar Rana <sharifzada586@gmail.com>
2023-09-12 11:30:43 +0500

    solve all problems useing forEach loop and function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...poped) {
+    const newArr = [];
+    array.forEach(Myfunction)
 
+    function Myfunction(value) {
+        if (!poped.includes(value)){
+            newArr.push(value)
+        }
+    };
+    return newArr;
 };
 
 // Do not edit below this line
