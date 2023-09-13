f17e71dbc07974c6ebcf7fd2a075781fdbbcde6a
Omar Rana <sharifzada586@gmail.com>
2023-09-13 11:15:15 +0500

    solved problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let result = (fahrenheit - 32) / 1.8;
+  let finel = parseFloat(result.toFixed(1));
+  return finel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius * 1.8) + 32;
+  let finel = parseFloat(result.toFixed(1));
+  return finel;
 };
 
 // Do not edit below this line
