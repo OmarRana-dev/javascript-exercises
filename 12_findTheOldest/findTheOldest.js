f40e4c952adedd52969d490d737dac36050e93e3
Omar Rana <sharifzada586@gmail.com>
2023-11-05 12:04:52 +0500

    done exersice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((personOne, personTwo) => {
+    let firstPerson = getAge(personOne.yearOfBirth, personOne.yearOfDeath);
+    let secondPerson = getAge(personTwo.yearOfBirth, personTwo.yearOfDeath);
 
+    return firstPerson > secondPerson ? personOne: personTwo;
+  });
 };
+
+function getAge(birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
