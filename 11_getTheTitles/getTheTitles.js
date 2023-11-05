80a1c5ec466ba5bdb1f072447690d139631b3a72
Omar Rana <sharifzada586@gmail.com>
2023-11-05 12:06:19 +0500

    done exersice

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let result = books.map(element => {
+        return element.title
+    });
 
+    return result;
 };
 
 // Do not edit below this line
